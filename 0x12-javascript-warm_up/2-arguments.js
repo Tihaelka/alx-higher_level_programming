54fda99c9a3d8fef9dc6e595bc03616ee1eb521d
Tihaelka <fkassimi50@gmail.com>
2024-01-08 06:51:21 -0800

    all

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
-
-// Check if there is at least one command line argument
-if (process.argv.length >= 3) {
-  const argumentToPrint = process.argv[2];
-  console.log("No argument:", argumentToPrint);
-} else {
-  console.log("argument found.");
-}
-
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
